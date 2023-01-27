59348a62c76e9a1d05a1ce798582ca9e34a42e86
susananzth <susananzth.dev@gmail.com>
2023-01-27 13:18:57 -0500

    Actualización y correcciones de autenticacion y test

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Auth\LoginRequest;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -18,13 +19,28 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) { 
+        if (Auth::attempt([
+            'email'    => $request->email, 
+            'password' => $request->password
+        ])) { 
+
             $user = Auth::user(); 
-            $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
-            $success['name'] =  $user->name;
-            return $this->sendResponse($success, 'User login successfully.');
+
+            $success['token'] = $user->createToken('MyApp')->plainTextToken; 
+            $success['name']  = $user->name;
+
+            return $this->sendResponse(
+                $success, 
+                'User login successfully.', 
+                Response::HTTP_OK);
+
         } else { 
-            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+
+            return $this->sendError(
+                'These credentials do not match our records.', 
+                ['error'=>'These credentials do not match our records.'], 
+                Response::HTTP_UNPROCESSABLE_ENTITY);
+
         } 
     }
 
@@ -34,6 +50,7 @@ class AuthenticatedSessionController extends Controller
     public function destroy(Request $request)
     {
         Auth::guard('web')->logout();
+        auth('sanctum')->user()->tokens()->delete();
 
         $request->session()->invalidate();
 
--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -2,36 +2,79 @@
 
 namespace Tests\Feature\Auth;
 
-use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AuthenticationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_users_can_authenticate_using_the_login_screen(): void
+    public function test_users_can_authenticate_using_email_and_password(): void
     {
         $user = User::factory()->create();
 
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password',
+        $input_data = [
+            'email'    => $user->email,
+            'password' => 'password'
+        ];
+        
+        $response = $this->json('post', 'api/login', $input_data);
+
+        $response->assertStatus(Response::HTTP_OK);
+
+        $response->assertJsonStructure([
+            'success',
+            'data'    => [
+                'token',
+                'name'
+            ],
+            'message',
         ]);
 
         $this->assertAuthenticated();
-        $response->assertNoContent();
+    }
+
+    public function test_users_can_not_authenticate_with_invalid_email(): void
+    {
+        $user = User::factory()->create();
+
+        $input_data = [
+            'email'    => 'wrong@email.com',
+            'password' => 'password'
+        ];
+
+        $response = $this->json('post', 'api/login', $input_data);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $response->assertJsonStructure([
+            "message",
+            "errors"
+        ]);
+
+        $this->assertGuest();
     }
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
     {
         $user = User::factory()->create();
 
-        $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'wrong-password',
-        ]);
+        $input_data = [
+            'email'    => $user->email,
+            'password' => 'wrong-password'
+        ];
 
+        $response = $this->json('post', 'api/login', $input_data);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        
+        $response->assertJsonStructure([
+            "message",
+            "errors"
+        ]);
+        
         $this->assertGuest();
     }
 }
